aab90db324f6b8265a9c7992a3cf498ba5c34cbe
Adoniran <adoniranfranca@gmail.com>
2025-07-10 22:32:22 -0300

    feat: criar componente Dashboard Vue

---
+++ app/javascript/components/dashboard/Dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <QuickActions />
+    <OverviewStats />
+    <div>
+      <div>
+        <RecentOrders />
+      </div>
+      <div>
+        <RestaurantInfo />
+        <PerformanceChart />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import QuickActions from './components/dashboard/QuickActions.vue';
+import OverviewStats from './components/dashboard/OverviewStats.vue'
+import RecentOrders from './components/dashboard/RecentOrders.vue'
+import RestaurantInfo from './components/dashboard/RestaurantInfo.vue'
+import PerformanceChart from './components/dashboard/PerformanceChart.vue'
+</script>
