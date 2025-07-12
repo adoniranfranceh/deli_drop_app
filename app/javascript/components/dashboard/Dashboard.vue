45dccf711366e7f39ec6aa689bceb0e6915f403c
Adoniran <adoniranfranca@gmail.com>
2025-07-12 01:32:01 -0300

    feat: estrutura e estilo iniciais da dashboard

--- app/javascript/components/dashboard/Dashboard.vue
+++ app/javascript/components/dashboard/Dashboard.vue
@@ -1,14 +1,25 @@
 <template>
-  <div class="dashboard-container">
-    <QuickActions />
-    <OverviewStats />
-    <div class="dashboard-main">
-      <div class="dashboard-left">
-        <RecentOrders />
-      </div>
-      <div class="dashboard-right">
-        <RestaurantInfo />
-        <PerformanceChart />
+  <div class="dashboard-header">
+    <h1 class="title">Dashboard</h1>
+    <p class="welcome-msg">Bem-vindo de volta, Hamburgueria Top</p>
+  </div>
+  <div class="dashboard">
+    <div class="dashboard-container">
+      <QuickActions />
+      <OverviewStats />
+      <div class="dashboard-main">
+        <div class="dashboard-left">
+          <RecentOrders />
+        </div>
+        <div class="dashboard-right">
+          <RestaurantInfo />
+          <PerformanceChart
+            :metrics="[
+              { label: 'Visualizações', total: 1000, value: 234, suffix: '' },
+              { label: 'Taxa de conversão', total: 100, value: 20.1, suffix: '%' },
+            ]"
+          />
+        </div>
       </div>
     </div>
   </div>
@@ -16,18 +27,47 @@
 
 <script setup>
 import QuickActions from './QuickActions.vue';
-import OverviewStats from './OverviewStats.vue'
-import RecentOrders from './RecentOrders.vue'
-import RestaurantInfo from './RestaurantInfo.vue'
-import PerformanceChart from './PerformanceChart.vue'
+import OverviewStats from './OverviewStats.vue';
+import RecentOrders from './RecentOrders.vue';
+import RestaurantInfo from './RestaurantInfo.vue';
+import PerformanceChart from './PerformanceChart.vue';
 </script>
 
 <style scoped>
+.dashboard-header {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 20rem;
+  margin: 3rem 0 0 3rem;
+}
+
+.title{
+  font-size: 30px;
+  font-weight: 700;
+  margin: 0;
+}
+
+.welcome-msg {
+  margin-top: 0.5rem;
+  font-weight: 400;
+  color: var(--color-muted);
+}
+
+.dashboard {
+  display: flex;
+  justify-content: center;
+}
+
 .dashboard-container {
   display: flex;
   flex-direction: column;
   padding: 2rem;
   gap: 2rem;
+  background-color: var(--color-background);
+  margin: 2rem;
+  width: 1820px;
 }
 
 .dashboard-main {
@@ -47,5 +87,17 @@ import PerformanceChart from './PerformanceChart.vue'
   display: flex;
   flex-direction: column;
   gap: 2rem;
+}
+@media (max-width: 900px) {
+  .dashboard-main{
+    flex-direction: column;
+  }
+}
+
+@media (max-width: 758px) {
+  .dashboard-container {
+    width: 100%;
+    margin: 0;
+  }
 }
 </style>
