343bcf6b2cf0cf2341f9c70a9d31f8a6e4c36a36
Adoniran <adoniranfranca@gmail.com>
2025-08-06 13:59:55 -0300

    feat(product-form): extrai primeiro passo do formulário para componente FormStepOne

--- app/javascript/components/products/ProductForm.vue
+++ app/javascript/components/products/ProductForm.vue
@@ -7,81 +7,18 @@
       { label: 'Revisão', icon: 'lucide:circle-check-big' }
     ]"
     :currentStep="step"
-    :canClick="canNavigateToStep"
+    :canClickSteps="canClickSteps"
     @update:step="step = $event"
   />
 
   <div class="product-form">
     <div class="form-container">
-      <form v-if="step === 1" @submit.prevent="submit" class="form-grid">
-        <h2 class="product-form-title">
-          <Icon icon="lucide:settings" /> Informações Básicas
-        </h2>
-
-        <div class="form-row">
-          <InputGroup
-            label="Nome"
-            placeholder="Nome do Produto"
-            v-model="product.name"
-            :externalError="productErrors?.name"
-            required
-          />
-          <InputDropdown
-            v-model="product.category"
-            :options="[
-              { label: 'Bebidas', value: 'bebidas' },
-              { label: 'Comidas', value: 'comidas' },
-              { label: 'Sobremesas', value: 'sobremesas' }
-            ]"
-            placeholder="Todas as categorias"
-            :externalError="productErrors?.category"
-            label="Categoria"
-            required
-          />
-        </div>
-
-        <div class="form-row">
-          <CurrencyInput
-            id="product-price"
-            label="Preço"
-            v-model="product.price"
-            :externalError="productErrors?.price"
-          />
-
-          <InputNumber
-            id="product-duration"
-            label="Tempo de preparo"
-            placeholder="Tempo de preparo (minutos)"
-            v-model="product.duration"
-            :min="1"
-            :max="1000"
-            :externalError="productErrors?.duration"
-            required
-          />
-        </div>
-
-        <InputGroup
-          id="product-description"
-          label="Descrição"
-          isTextarea
-          placeholder="Descreva o produto..."
-          v-model="product.description"
-          required
-        />
-
-        <IngredientsInput v-model="product.ingredients" />
-
-        <div class="switch-wrapper">
-          <ToggleSwitch
-            v-model="product.isActive"
-            label="Produto ativo"
-          />
-          <ToggleSwitch
-            v-model="product.isFeatured"
-            label="Produto em destaque"
-          />
-        </div>
-      </form>
+      <QuickTemplate @update:product="fillProduct" v-if="step === 1"/>
+      <FormStepOne
+        v-if="step === 1"
+        :product="product"
+        :errors="productErrors"
+      />
       <ModifierOptionsPrompt
         v-if="isProductValid && step === 1"
         @modifiers-decision="handleStepChange"
@@ -89,17 +26,14 @@
       <ModifierGroup
         v-if="step === 2"
         v-model="product.modifiers_groups"
-        @next="step = 3"
-        @validation-state="isGroupsValid = $event"
       />
       <div class="form-actions">
-        <AppButton class="cancel" @click="navigateTo('/menu')" text="Cancelar" iconLeft="ic:round-close" v-if="step === 1"/>
         <AppButton
           class="cancel"
           @click="step = step > 1 ? step - 1 : 1"
           text="Voltar"
           iconLeft="lucide-arrow-left"
-          v-else
+          v-if="step > 1"
         />
         <AppButton
           class="save"
@@ -109,7 +43,6 @@
           :disabled="isNextDisabled"
           v-if="step < 3"
         />
-
         <AppButton
           class="save"
           text="Salvar"
@@ -160,16 +93,6 @@ const { fillProduct } = useProductTemplate(product)
   justify-content: center;
 }
 
-.product-form-title {
-  display: flex;
-  align-items: center;
-  gap: 0.5rem;
-  font-weight: bold;
-  margin: 0;
-  justify-content: center;
-  margin-bottom: 1.5rem;
-}
-
 .form-container {
   display: flex;
   flex-direction: column;
@@ -179,29 +102,6 @@ const { fillProduct } = useProductTemplate(product)
   background-color: var(--color-background);
   margin: 0 2rem;
   width: 1820px;
-}
-
-.form-grid {
-  display: flex;
-  flex-direction: column;
-  gap: 1.2rem;
-  width: 80%;
-  padding: 2rem;
-  background: var(--color-white);
-  border: 1px solid var(--color-border);
-  border-radius: 8px;
-}
-
-.form-row {
-  display: flex;
-  gap: 1.5rem;
-  flex-wrap: wrap;
-}
-
-.switch-wrapper {
-  display: flex;
-  gap: 1.5rem;
-  align-items: center;
 }
 
 .form-actions {
@@ -242,21 +142,6 @@ button.cancel:hover {
     width: 100%;
     margin: 0;
     padding: 0.5rem;
-  }
-
-  .form-row {
-    flex-direction: column;
-  }
-
-  .form-grid {
-    width: 100%;
-    padding: 0.4rem;
-    margin: 0;
-  }
-
-  .switch-wrapper {
-    align-items: flex-start;
-    gap: 1rem;
   }
 
   .form-actions {
