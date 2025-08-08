f1b1b44ba8aab1d09d0ebb51718cabb53bb99703
Adoniran <adoniranfranca@gmail.com>
2025-08-08 17:55:34 -0300

    feat(product-step3): adiciona componente ModifierGroup para gerenciamento de grupos de modificadores

---
+++ app/javascript/components/products/StepThree/ModifierGroup.vue
@@ -0,0 +1,29 @@
+<script setup>
+import ModifierCheckbox from './ModifierCheckbox.vue'
+import ModifierRadio from './ModifierRadio.vue'
+import ModifierQuantity from './ModifierQuantity.vue'
+
+const props = defineProps({
+  modifier_group: Object,
+})
+
+const emit = defineEmits(['update:selected'])
+
+const componentMap = {
+  multiple_choice: ModifierCheckbox,
+  single_choice: ModifierRadio,
+  quantity: ModifierQuantity
+}
+
+const updateSelected = (payload) => {
+  emit('update:selected', payload)
+}
+</script>
+
+<template>
+  <component
+    :is="componentMap[modifier_group.input_type]"
+    :modifier_group="modifier_group"
+    @update:selected="updateSelected"
+  />
+</template>
