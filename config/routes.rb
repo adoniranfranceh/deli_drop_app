547345a19b222baeef2f03118f0dc84deba4b16a
Adoniran <adoniranfranca@gmail.com>
2025-08-29 14:06:26 -0300

    feat(flash): permite que flash receba mensagem via endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :restaurants, only: %i[create update ]
+      resources :restaurants, only: %i[create update]
     end
   end
+
+  get "/flash", to: "flash#redirect"
 end
