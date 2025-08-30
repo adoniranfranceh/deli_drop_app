f7e68171e29a8df49e5aea8a9612cb07b712b5b3
Adoniran <adoniranfranca@gmail.com>
2025-08-30 17:47:01 -0300

    feat(restaurant): adicionar validações de presença e unicidade
    - Requer nome, tipo de culinária, descrição, imagem e telefone
    - Garante unicidade do restaurant_user_id
    - Define enum para culinary_style

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,10 @@
 class Restaurant < ApplicationRecord
   belongs_to :restaurant_user
+
+  validates :name, :culinary_style, :description, :image, :phone, presence: true
+  validates :restaurant_user_id, uniqueness: true
+
+  enum :culinary_style, { acai: 0, brazilian: 1, italian: 2, japanese: 3, mexican: 4,
+                        burgers: 5, pizza: 6, healthy: 7, vegetarian: 8, sweets: 9,
+                        snacks: 10, barbecue: 11, seafood: 12, coffee_shop: 13, ice_cream: 14 }
 end
