c7e1fd230f1722e2a5213316c504cc1d65f1df0d
Adoniran <adoniranfranca@gmail.com>
2025-09-04 18:33:27 -0300

    feat(auth): integrate Pundit into ApplicationController
    - include Pundit::Authorization
    - define `pundit_user` as `current_restaurant_user`
    - rescue_from Pundit::NotAuthorizedError
    - rename before_action from `restaurant_user_signed_in?` to `restaurant_registered?`

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,24 @@
 class ApplicationController < ActionController::Base
   allow_browser versions: :modern
   before_action :authenticate_restaurant_user!
-  before_action :restaurant_user_signed_in?
+  before_action :restaurant_registered?
+
+  include Pundit::Authorization
+
+  def pundit_user
+    current_restaurant_user
+  end
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
 
-  def restaurant_user_signed_in?
+  def user_not_authorized
+    flash[:alert] = I18n.t "alerts.pundit.default", default:
+    redirect_to(request.referer || root_path)
+  end
+
+  def restaurant_registered?
     if current_restaurant_user && current_restaurant_user.restaurant.nil?
       unless flash[:alert].present? || flash[:notice].present?
         redirect_to new_restaurant_path, alert: I18n.t("alerts.restaurant.must_register")
