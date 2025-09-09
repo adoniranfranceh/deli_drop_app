02e83f70d8d767c35043553c0fd8d84e3ddfcdcb
Adoniran <adoniranfranca@gmail.com>
2025-09-09 16:47:09 -0300

    test(policy/restaurant): add specs for update? and translate to English

--- spec/policies/restaurant_policy_spec.rb
+++ spec/policies/restaurant_policy_spec.rb
@@ -2,14 +2,14 @@ require "rails_helper"
 
 RSpec.describe RestaurantPolicy do
   describe "#new?" do
-    it "permite criar se usuário não tiver restaurante" do
+    it "allows creating if user not has restaurant" do
       user = create(:restaurant_user, restaurant: nil)
 
       policy = described_class.new(user, Restaurant)
       expect(policy.new?).to be true
     end
 
-    it "nega criar se usuário já tiver restaurante" do
+    it "denies creating if user already has a restaurant" do
       user = create(:restaurant_user)
       create(:restaurant, restaurant_user: user)
 
@@ -19,7 +19,7 @@ RSpec.describe RestaurantPolicy do
   end
 
   describe "#edit?" do
-    it "permite dono editar seu próprio restaurante" do
+    it "allows owner editing their own restaurant" do
       user = create(:restaurant_user)
       restaurant = create(:restaurant, restaurant_user: user)
 
@@ -27,13 +27,32 @@ RSpec.describe RestaurantPolicy do
       expect(policy.edit?).to be true
     end
 
-    it "nega edição se não for o dono" do
+    it "denies editing if not the owner" do
       user = create(:restaurant_user)
       other_user = create(:restaurant_user, email: 'other@email.com')
       restaurant = create(:restaurant, restaurant_user: user)
 
       policy = described_class.new(other_user, restaurant)
       expect(policy.edit?).to be false
+    end
+  end
+
+  describe "#update?" do
+    it "allows owner updating their own restaurant" do
+      user = create(:restaurant_user)
+      restaurant = create(:restaurant, restaurant_user: user)
+
+      policy = described_class.new(user, restaurant)
+      expect(policy.update?).to be true
+    end
+
+    it "denies updating if not the owner" do
+      user = create(:restaurant_user)
+      other_user = create(:restaurant_user, email: 'other@email.com')
+      restaurant = create(:restaurant, restaurant_user: user)
+
+      policy = described_class.new(other_user, restaurant)
+      expect(policy.update?).to be false
     end
   end
 end
