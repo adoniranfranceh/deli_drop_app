1be06d49c2f4c9768574df0425a92650be4398d1
Adoniran <adoniranfranca@gmail.com>
2025-09-24 16:24:28 -0300

    feat(ui): make flash messages fixed and closable
    - Added AppButton with x to close flash
    - Set position fixed with high z-index
    - Added ref `visible` to hide flash on close

--- app/javascript/components/ui/FlashMessage.vue
+++ app/javascript/components/ui/FlashMessage.vue
@@ -1,31 +1,45 @@
 <script setup>
-import { Icon } from '@iconify/vue';
+import { ref } from 'vue'
+import { Icon } from '@iconify/vue'
+import AppButton from './AppButton.vue'
 
 defineProps({
   type: { type: String, required: true },
   message: { type: String, required: true }
-});
+})
+
+const visible = ref(true)
+const close = () => {
+  visible.value = false
+}
 </script>
 
 <template>
-  <div class="mensages">
+  <div v-if="visible" class="messages">
     <div :class="['app-flash', `app-flash--${type}`]">
       <span class="app-flash__icon">
         <Icon v-if="type === 'success'" icon="fa6-solid:check" />
         <Icon v-else icon="lucide:triangle-alert" />
       </span>
       <span class="app-flash__text">{{ message }}</span>
+
+      <AppButton
+        icon="fa6-solid:xmark"
+        class="app-flash__close"
+        @click="close"
+      />
     </div>
   </div>
 </template>
 
 <style scoped>
-.mensages {
+.messages {
   width: 100%;
   display: flex;
   justify-content: center;
-  position: absolute;
+  position: fixed;
   top: 1rem;
+  z-index: 9999;
 }
 
 .app-flash {
@@ -42,6 +56,22 @@ defineProps({
 
 .app-flash__icon {
   font-size: 1.2rem;
+}
+
+.app-flash__close {
+  background: none;
+  color: inherit;
+  padding: 0.3rem;
+}
+
+.app-flash__close svg {
+  width: 0.7rem;
+  height: 0.7rem;
+}
+
+.app-flash__close:hover {
+  background: none;
+  opacity: 0.7;
 }
 
 .app-flash--success {
