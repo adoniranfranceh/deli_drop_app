c731929dcc270051956fb70ba3d908d8410ffd5c
Adoniran <adoniranfranca@gmail.com>
2025-09-24 16:23:03 -0300

    feat(categories): include products stats on create and update
    - Updated CategoriesController to return category.with_products_stats
      on create and update actions

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -8,7 +8,10 @@ class Api::V1::CategoriesController < Api::V1::ApplicationController
     category = Category.new(category_params)
 
     if category.save
-      render json: { message: I18n.t("api.v1.categories.create.success"), category: category }, status: :created
+      render json: {
+        message: I18n.t("api.v1.categories.create.success"),
+        category: category.with_products_stats
+      }, status: :created
     else
       render json: { status: :unprocessable_entity, errors: category.errors.full_messages.join(", ") }, status: :unprocessable_entity
     end
@@ -19,7 +22,10 @@ class Api::V1::CategoriesController < Api::V1::ApplicationController
     authorize category
 
     if category.update(category_params)
-      render json: { message: I18n.t("api.v1.categories.update.success"), category: }, status: :ok
+      render json: {
+        message: I18n.t("api.v1.categories.update.success"),
+        category: category.with_products_stats
+      }, status: :ok
     else
       render json: { status: :unprocessable_entity, errors: category.errors.full_messages.join(", ") }, status: :unprocessable_entity
     end
