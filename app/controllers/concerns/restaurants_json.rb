a29e844b62a0d029ada066a2df08761389b2b281
Adoniran <adoniranfranca@gmail.com>
2025-10-08 17:58:24 -0300

    feat(restaurants-json): allow restaurants_json to receive collection

--- app/controllers/concerns/restaurants_json.rb
+++ app/controllers/concerns/restaurants_json.rb
@@ -2,16 +2,17 @@ module RestaurantsJson
   extend ActiveSupport::Concern
   include RestaurantJson
 
-  def restaurants_json(full: false)
-    restaurants = Restaurant.joins(:products).distinct.order(:id)
+  def restaurants_json(full: false, collection: nil)
+    collection ||= Restaurant.all
 
-    restaurants.map do |restaurant|
+    collection.map do |restaurant|
       if full
         restaurant_json(restaurant)
       else
         {
           id: restaurant.id,
           name: restaurant.name,
+          description: restaurant.description,
           logo: restaurant.image
         }
       end
