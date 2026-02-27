f28473129cb932d1c3a3046772a0de197d16f26f
adoniranfranceh <116985618+adoniranfranceh@users.noreply.github.com>
2026-02-27 00:26:28 -0300

    feat: enhance ActionCable connection to include guest ID for unauthenticated users

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,15 +1,10 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    identified_by :restaurant_user
+    identified_by :restaurant_user, :guest_id
 
     def connect
-      self.restaurant_user = find_restaurant_user
-    end
-
-    private
-
-    def find_restaurant_user
-      env["warden"]&.user(:restaurant_user)
+      self.restaurant_user = env["warden"]&.user(:restaurant_user)
+      self.guest_id = SecureRandom.uuid unless restaurant_user
     end
   end
 end
